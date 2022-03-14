0e8832b944ca33b1157876c0f249dd322e11b959
Svante Pålsson <svantepalsson1987@gmail.com>
2022-03-14 09:11:14 +0100

    Add project files.

---
+++ Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using InspectionAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InspectionAPI.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Inspection> Inspections { get; set; }
+
+        public DbSet<InspectionType> Types { get; set; }
+
+        public DbSet<Status> Statuses { get; set; }
+
+    }
+}
